a3c484a81317ceb6a915c1889f834434cee92455
hejtmy <hejtmy@gmail.com>
2016-04-20 10:22:30 +0200

    Adds class to hold multiple participant classes as well as basic manipulation with behavioral data

---
+++ Scripts/Classes/MultiParticipantUnityAnalysis.R
@@ -0,0 +1,27 @@
+library('R6')
+MultiParticipantUnityAnalysis <- R6Class("MultiParticipantUnityAnalysis",
+                                         
+ #define variables
+ public = list(
+    Analyses = NULL,
+    initialize = function(dir, participants,session){
+      Analyses = list()
+      for(i in 1:length(participants)){
+        participant_code = participants[i]
+        analysis = UnityEyetrackerAnalysis$new(dir,participant_code,session)
+        analysis$ReadData()
+        self$Analyses[[participant_code]] = analysis
+      }
+   },
+   QuestsSummary = function(){
+     final = data.frame()
+     for(i in 1:length(self$Analyses)){
+       ana = self$Analyses[[i]]
+       df = ana$QuestsSummary()
+       df = mutate(df, participant_id = rep(ana$id,nrow(df)))
+       final = rbindlist(list(final,df))
+     }
+     return(final)
+   }
+ )
+)
