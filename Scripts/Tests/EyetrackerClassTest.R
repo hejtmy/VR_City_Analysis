79e6af23cae9747bfd1e01b8a4e66fbac0ab4266
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-07-16 01:31:52 +0200

    Adds testing script

---
+++ Scripts/Tests/EyetrackerClassTest.R
@@ -0,0 +1,14 @@
+dir = "U:/OneDrive/NUDZ/HCENAT/Data/"
+participant_code = "HCE_E_2"
+edf_code = "HCE_1E2"
+override = F
+save = T
+eye = EyetrackerAnalysis$new(dir, participant_code, edf_code, override, save)
+
+unity_class = UnityEyetrackerAnalysis$new(data_dir, id = participant_code, session = 1)
+
+eye$summary(quest_times = unity_class$quests_timewindows(T))
+
+events = eye$events
+fixations = eye$fixations
+quest_times = unity_class$quests_timewindows(T)
