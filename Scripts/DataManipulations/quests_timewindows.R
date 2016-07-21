94dac52d532e97f728364182ed601fd52a32d5f9
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-07-22 01:55:43 +0200

    fixes copying of dt

--- Scripts/DataManipulations/quests_timewindows.R
+++ Scripts/DataManipulations/quests_timewindows.R
@@ -1,5 +1,4 @@
 quests_timewindows = function(quest_set, trial_sets, include_teleport = T){
-  df = select(quest_set, id:name)
   n_quests = nrow(quest_set)
   df_times = data.frame(starts = numeric(n_quests), ends = numeric(n_quests))
   for(i in 1:nrow(quest_set)){
@@ -9,6 +8,6 @@ quests_timewindows = function(quest_set, trial_sets, include_teleport = T){
     df_times$starts[i] = times$start
     df_times$ends[i] = times$finish
   }
-  df = bind_cols(df, df_times)
-  return(df)
+  quest_set[,`:=`(starts=df_times$starts, ends=df_times$ends)]
+  return(quest_set)
 }
