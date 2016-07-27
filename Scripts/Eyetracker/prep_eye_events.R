6af531679cfd8e49c335296d84c858e0670d0418
hejtmy <hejtmy@gmail.com>
2016-07-27 10:23:41 +0200

    Adds removal of walking events and duplicit KeyUP down events

--- Scripts/Eyetracker/prep_eye_events.R
+++ Scripts/Eyetracker/prep_eye_events.R
@@ -1,15 +1,24 @@
 prep_eye_events = function(tab){
   #checks if it has already been preprocessed
   ls = list()
+  return_tab = tab
   ls$changed = F
-  if(grepl("\\[", tab[1, type])){
+  if(grepl("\\[", return_tab[1, type])){
     rm_brackets = function(x) gsub("\\[|\\]", "", x)
-    tab[, type := sapply(type, rm_brackets)]
+    return_tab = return_tab[, type := sapply(type, rm_brackets)]
     ls$changed = T
   }
-  if(tab[type != 'l', .N] > 0){
-    ls$result = tab[name == "KEY_UP" & type == 'l']
+  #removing key down
+  if(return_tab[name == "KEY_DOWN", .N] > 0){
+    return_tab = return_tab[name == "KEY_UP",]
     ls$changed = T
   }
+  #removing walking keys
+  walking_keys = c('w','a','s','d')
+  if(return_tab[type %in% walking_keys, .N] > 0){
+    return_tab = return_tab[!(type %in% walking_keys)]
+    ls$changed = T
+  }
+  ls$result = return_tab
   return(ls)
 }
