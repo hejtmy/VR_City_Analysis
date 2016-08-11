99dfe10322eebb099031a0eea4c0f2f49d34e5df
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 13:27:28 +0200

    Adds exception to fix missing player logs

--- Scripts/DataManipulations/quest_start_finish_positions.R
+++ Scripts/DataManipulations/quest_start_finish_positions.R
@@ -6,6 +6,7 @@ quest_start_finish_positions = function(quest_set, trial_sets, quest, include_te
   ls = list()
   time_teleport_finished = quest_timewindow(quest, include_teleport = include_teleport)$start
   ls[["start"]] = player_position_at_time(quest_set, trial_sets, quest, time_teleport_finished)
+  if(is.null(ls[["start"]])) return(NULL)
   ls[["finish"]] = LastQuestPosition(quest)
   return(ls)
 }
