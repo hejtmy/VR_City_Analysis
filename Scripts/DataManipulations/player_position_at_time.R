89617eda899009a94a4594b9109a86ec5b3a530b
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 11:52:13 +0200

    renames function used relative to the updated function

--- Scripts/DataManipulations/player_position_at_time.R
+++ Scripts/DataManipulations/player_position_at_time.R
@@ -1,6 +1,6 @@
 player_position_at_time = function(quest_set, trial_sets, quest, time){
-  log = trial_player_log_quest(quest_set, quest, trial_sets)
-  position = log[Time > time, .SD[1,c(Position.x,Position.z)]]
+  log = player_log_quest_trial(quest_set, quest, trial_sets)
+  position = log[Time > time, .SD[1, c(Position.x, Position.z)]]
   return(position)
 }
 
