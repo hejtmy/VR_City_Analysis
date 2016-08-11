62a39df41e45fb88d27cf203e35c22c290a1fb34
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 14:01:34 +0200

    Adds shortes trail to end to close #6

--- Scripts/Preprocessing/MakeQuestsSummary.R
+++ Scripts/Preprocessing/MakeQuestsSummary.R
@@ -15,14 +15,17 @@ MakeQuestsSummary = function(quest_set, trial_sets){
   trail_distances = numeric(nrow(df))
   trail_finished = logical(nrow(df))
   trailDistancesToEnd = numeric(nrow(df))
+  quest_sky_distance = numeric(nrow(df))
   for(i in 1:nrow(df)){
     quest_summary = MakeQuestSummary(quest_set, trial_sets, quest_order_session = i) #possble to get NULL
     trail_times[i] = ifelse(length(quest_summary$Time) < 1, NA, quest_summary$Time)
     trail_distances[i] = ifelse(length(quest_summary$Distance) < 1, NA, quest_summary$Distance)
     trail_finished[i] = quest_summary$Finished
     trailDistancesToEnd[i] = ifelse(length(quest_summary$DistanceToLastStep) < 1, NA, quest_summary$DistanceToLastStep)
+    quest_sky_distance[i] = ifelse(is.null(quest_summary$sky_distance), NA, quest_summary$sky_distance)
   }
-  df = mutate(df, time = trail_times, distance = trail_distances, finished = trail_finished, trailDistanceToEnd = trailDistancesToEnd)
+  df = mutate(df, time = trail_times, distance = trail_distances, finished = trail_finished, 
+              trailDistanceToEnd = trailDistancesToEnd, skyDistance = quest_sky_distance)
   return(df)
 }
 
