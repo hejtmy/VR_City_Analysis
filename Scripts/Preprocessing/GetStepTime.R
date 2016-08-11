65cff9d467333857e9d498768f3259e72a3224cf
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 11:51:42 +0200

    adds helper function to collect informatio aobut all steps of certain type

--- Scripts/Preprocessing/GetStepTime.R
+++ Scripts/Preprocessing/GetStepTime.R
@@ -15,9 +15,35 @@ GetStepTime = function(quest, step_name, step_action = "StepActivated", step_id 
   if(length(stepTime) == 0) return(NULL)
   return(stepTime)
 }
+get_steps_times = function(quest, name, action = "StepActivated"){
+  if(is.null(quest)){
+    print("Quest log not reachable")
+    return(NULL)
+  }
+  step_times = quest$data[quest$data$StepType == name & quest$data$Action == step_action, c("StepID", "TimeFromStart")]
+  return(step_times)
+}
+
+#' returns data frame with all occurances of certain step type in a form of StepID, StepActivated, StepFinished
+#' 
+#' @param quest Loaded quest list as by get_quest
+#' @param name Name of the step, ex. "Point in Direction"
+get_step_timespans = function(quest, name){
+  if(is.null(quest)){
+    print("Quest log not reachable")
+    return(NULL)
+  }
+  step_times = quest$data[quest$data$StepType == name, c("TimeFromStart", "Action", "StepID")]
+  if(nrow(step_times) < 1 ) return(NULL)
+  step_times = reshape(step_times, timevar = c("Action"), idvar = "StepID", direction = "wide")
+  colnames(step_times) = c("StepID", "StepActivated", "StepFinished")
+  return(step_times)
+}
+
 GetLastStepTime = function(quest, stepAction = "StepFinished"){
   return(GetStepTime(quest, step_action = stepAction, step_id = max(quest$steps$ID)))
 }
+
 GetQuestEndTime = function(quest){
   stepTime = quest$data$TimeFromStart[quest$data$Action == "Quest finished"]
   if(length(stepTime) > 1){ 
