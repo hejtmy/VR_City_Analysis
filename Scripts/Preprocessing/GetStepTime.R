42afe63687a79e381ebe066e9446600d999bd795
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 12:40:17 +0200

    Adds exception for unfinished quests

--- Scripts/Preprocessing/GetStepTime.R
+++ Scripts/Preprocessing/GetStepTime.R
@@ -35,7 +35,12 @@ get_step_timespans = function(quest, name){
   }
   step_times = quest$data[quest$data$StepType == name, c("TimeFromStart", "Action", "StepID")]
   if(nrow(step_times) < 1 ) return(NULL)
+  # it is possible to extract only one part of the quest (e.g. was only activated or only finnished)
   step_times = reshape(step_times, timevar = c("Action"), idvar = "StepID", direction = "wide")
+  if(ncol(step_times) != 3){
+    SmartPrint(c("WARNING:get_step_timespans", "TYPE:Step", name, "incomplete", "QUEST:", quest$name, "DESCRIPTION: Step doesn't have complete acitvation and finish", "ACTION: Skipping"))
+    return(NULL)
+  }
   colnames(step_times) = c("StepID", "StepActivated", "StepFinished")
   return(step_times)
 }
