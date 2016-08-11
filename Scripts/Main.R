e17c5bf24dd32db2aa8e8eee56aa0b00986bb145
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-11 13:28:48 +0200

    Adds function calls

--- Scripts/Main.R
+++ Scripts/Main.R
@@ -7,18 +7,23 @@ data_dir= "U:/OneDrive/NUDZ/HCENAT/Data/"
 
 subject_table = read.table(paste(data_dir, "ListOfSubjects.csv",sep=""), sep = ",", header=T, stringsAsFactors = F, na.strings = c(""))
 
+subject_table = subject_table[1:10,]
+
 #instantiates VR_analysis class with the name and project directory 
 #it loads appropriate log files and allows for immediate analysis
-Analysis = UnityEyetrackerAnalysis$new(data_dir, id="HCE_E_12", session=1)
-Analysis$QuestSummary(7)
-Analysis$QuestsSummary()
-Analysis$DrawQuestPath(2)
+AnalysisEye = UnityEyetrackerAnalysis$new(data_dir, id = "HCE_E_10", session = 1)
+AnalysisEye$QuestSummary(7)
+AnalysisEye$QuestsSummary()
+AnalysisEye$DrawQuestPath(2)
+AnalysisEye$pointing_summary()
 
 #MRI analysis
-Analysis = UnityMRIAnalysis$new(data_dir, id="HCE_K_10", session=1)
-Analysis$QuestSummary(2)
-Analysis$QuestsSummary()
-Analysis$DrawQuestPath(1)
+AnalysisMRI = UnityMRIAnalysis$new(data_dir, id = "HCE_E_10", session = 1)
+AnalysisMRI$QuestSummary(2)
+AnalysisMRI$QuestsSummary()
+AnalysisMRI$DrawQuestPath(1)
+AnalysisMRI$pointing_summary()
+
 
 #loads from the subjectList table
 Analyses = MultiParticipantUnityAnalysis$new(data_dir, subject_table, 1, override = F)
@@ -26,10 +31,12 @@ tabEyeQuests = Analyses$EyetrackerQuestsSummary()
 tabMRIQuests = Analyses$MRIQuestSummary()
 tabMRIPulses = Analyses$SynchropulsesTable()
 synced_fixations = Analyses$synchronise_eyetracker()
+dt_pointing = Analyses$pointing_summary()
 
 write.table(tabEyeQuests,"tabEyeQuests.csv", sep=";", row.names = F, quote = F)
 write.table(tabMRIQuests,"tabMRIQuests.csv", sep=";", row.names = F, quote = F)
-write.table(synced_fixations,"synced_fixations", sep=";", row.names = F, quote = F)
+write.table(synced_fixations,"synced_fixations.csv", sep=";", row.names = F, quote = F)
+write.table(dt_pointing,"pointing.csv", sep=";", row.names = F, quote = F)
 
 MRIInformation(tabMRIPulses)
 
