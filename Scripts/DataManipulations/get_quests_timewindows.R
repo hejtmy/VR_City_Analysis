143c123915881272ea4a0a0eaf9a60b41c925f06
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-19 17:05:45 +0200

    fixes an issue with the dt wehere passing by reference modified the original quest_set

--- Scripts/DataManipulations/get_quests_timewindows.R
+++ Scripts/DataManipulations/get_quests_timewindows.R
@@ -1,13 +1,15 @@
 get_quests_timewindows = function(quest_set, trial_sets, include_teleport = T){
-  n_quests = nrow(quest_set)
+  
+  dt = copy(quest_set)
+  n_quests = nrow(dt)
   df_times = data.frame(starts = numeric(n_quests), ends = numeric(n_quests))
-  for(i in 1:nrow(quest_set)){
-    line = quest_set[i,]
-    quest = get_quest(quest_set, trial_sets, line$order_session)
+  for(i in 1:nrow(dt)){
+    line = dt[i,]
+    quest = get_quest(dt, trial_sets, line$order_session)
     times = quest_timewindow(quest, include_teleport = include_teleport)
     df_times$starts[i] = times$start
     df_times$ends[i] = times$finish
   }
-  quest_set[,`:=`(starts=df_times$starts, ends=df_times$ends)]
-  return(quest_set)
+  dt[,`:=`(starts=df_times$starts, ends=df_times$ends)]
+  return(dt)
 }
