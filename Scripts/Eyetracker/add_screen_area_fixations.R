46ce04f618c52074d8ccf3181e8a4b705fa0f02d
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-08-30 14:30:08 +0200

    adds clear function to encapsulate script

--- Scripts/Eyetracker/add_screen_area_fixations.R
+++ Scripts/Eyetracker/add_screen_area_fixations.R
@@ -8,22 +8,27 @@
 #' @param areas vector of lists with name and vector of points
 #' 
 
-height = 1080
-width = 1920
-quater = function(num){return(num/4)}
-third = function(num){return(num/3)}
-around = function(middle, deviation){return(c(middle - deviation, middle + deviation))}
-map_area = list(name = "map", x = c(0, quater(width)), y = c(0, quater(height)))
-center_area = list(name = "center", x = around(width/2, quater(width)/2), 
-                                    y = around(height/2, quater(height)/2))
-control_area = list(name = "control",x = c(0, quater(width)),
-                                     y = c(height, height - quater(height)))
-
-areas = list(map_area, center_area, control_area)
+create_areas = function(height, width){
+  #assigns height width
+  HEIGHT = 1080
+  WIDTH = 1920
+  quater = function(num){return(num/4)}
+  third = function(num){return(num/3)}
+  around = function(middle, deviation){return(c(middle - deviation, middle + deviation))}
+  map_area = list(name = "map", x = c(0, quater(WIDTH)), y = c(0, quater(HEIGHT)))
+  center_area = list(name = "center", x = around(WIDTH/2, quater(WIDTH)/2), 
+                     y = around(HEIGHT/2, quater(HEIGHT)/2))
+  control_area = list(name = "control",x = c(0, quater(WIDTH)),
+                      y = c(HEIGHT, HEIGHT - quater(HEIGHT)))
+  
+  areas = list(map_area, center_area, control_area)
+  
+}
 
 add_screen_area_fixations = function(dt_fixations, areas){
   dt_fixations[, area := ""]
   for (ar in areas){
     dt_fixations[is_between(pos_x, ar$x[1], ar$x[2]) & is_between(pos_y, ar$y[1], ar$y[2]), area:= ar$name]
   }
+  return(dt_fixations)
 }
