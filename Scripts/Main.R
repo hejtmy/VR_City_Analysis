cb8ce4de4747abf10f5b893895dccf51d4249606
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-10-09 02:09:52 +0200

    cleans up

--- Scripts/Main.R
+++ Scripts/Main.R
@@ -1,26 +1,29 @@
 source("Scripts/LoadingScript.R")
 
 #sets the working dirrectory where the logs are
-data_dir <- "../Data/"
+data_dir = "../Data/"
 data_dir = "C:/Users/hejtmy/OneDrive/NUDZ/HCENAT/Data/"
-data_dir= "U:/OneDrive/NUDZ/HCENAT/Data/"
+data_dir = "U:/OneDrive/NUDZ/HCENAT/Data/"
 
-subject_table = read.table(paste(data_dir, "ListOfSubjects.csv",sep=""), sep = ",", header=T, stringsAsFactors = F, na.strings = c(""))
+subject_table = read.table(paste(data_dir, "ListOfSubjects.csv",sep = ""), sep = ",", 
+                           header = T, stringsAsFactors = F, na.strings = c(""))
+
+SESSION = 1
 
 #loads from the subjectList table
-Analyses = MultiParticipantUnityAnalysis$new(data_dir, subject_table, 2, override = F)
+Analyses = MultiParticipantUnityAnalysis$new(data_dir, subject_table, SESSION, override = F)
 tabEyeQuests = Analyses$EyetrackerQuestsSummary()
 tabMRIQuests = Analyses$MRIQuestSummary()
 tabMRIPulses = Analyses$SynchropulsesTable()
 synced_fixations = Analyses$synchronise_eyetracker()
 dt_pointing = Analyses$pointing_summary()
 
-write.table(tabEyeQuests,"tabEyeQuests_session2.csv", sep=";", row.names = F, quote = F)
-write.table(tabMRIQuests,"tabMRIQuests_session2.csv", sep=";", row.names = F, quote = F)
-write.table(synced_fixations,"synced_fixations_session2.csv", sep=";", row.names = F, quote = F)
-write.table(dt_pointing,"pointing_session2.csv", sep=";", row.names = F, quote = F)
+write.table(tabEyeQuests,"tabEyeQuests_session2.csv", sep = ";", row.names = F, quote = F)
+write.table(tabMRIQuests,"tabMRIQuests_session2.csv", sep = ";", row.names = F, quote = F)
+write.table(synced_fixations,"synced_fixations_session2.csv", sep = ";", row.names = F, quote = F)
+write.table(dt_pointing,"pointing_session2.csv", sep = ";", row.names = F, quote = F)
 
 areas = create_areas()
 dt = add_screen_area_fixations(synced_fixations, areas)
 fix_summary = make_fixations_summary(dt)
-write.table(fix_summary,"fixations_summary_session2.csv", sep=";", row.names = F, quote = F)
+write.table(fix_summary, "fixations_summary_session2.csv", sep = ";", row.names = F, quote = F)
