032eb0fe53de2ebf97d077d8541d9926bf111889
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-11-28 00:33:34 +0100

    adds null checking to mri pulses table

--- Scripts/MRI/mri_pulses_table.R
+++ Scripts/MRI/mri_pulses_table.R
@@ -1,5 +1,8 @@
 mri_pulses_table = function(MRIAnalysis){
   player_log = MRIAnalysis$trial_sets[[1]]$player_log
+  if(is.null(player_log)) return(NULL)
   pulsesTable = player_log[Input == "fMRISynchro"]
+  #sometimes we want to find those pulses where event begun
   return(pulsesTable)
 }
+
