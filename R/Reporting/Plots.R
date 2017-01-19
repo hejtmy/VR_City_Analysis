dbe899c2972af661b220d1379c47622be83e682e
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-01-19 18:51:13 +0100

    Removes redundant files

--- R/Reporting/Plots.R
+++
@@ -1,6 +0,0 @@
-CompareMeansPlot = function(pltTab, what = NULL ){
-  plt = ggplot(pltTab, aes_string(y='mean',x=what))
-  plt = plt +  geom_bar(aes_string(fill = what), stat='identity')
-  plt = plt + geom_errorbar(aes(ymin=mean-se, ymax=mean+se),size=.3,width=0.4,position=position_dodge(.9))
-  return(plt)
-}
