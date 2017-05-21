ef3eb0a4365059bc49e2a52c465293edd2c2750c
hejtmy <hejtmy@gmail.com>
2017-05-21 23:30:31 +0200

    moves addingpoints to separate function

--- R/Graphing/make_path_image.R
+++ R/Graphing/make_path_image.R
@@ -1,4 +1,4 @@
-make_path_image = function (img_location, position_table, map_size, special_points = NULL){
+make_path_image = function (img_location, position_table, map_size){
   
   img = readPNG(img_location)
   
@@ -16,20 +16,5 @@ make_path_image = function (img_location, position_table, map_size, special_poin
   } else {
     plot = plot + geom_path(size = 1)
   }
-  if (!is.null(special_points)){
-    plot = AddPointsToPlot(plot, special_points)
-  }
-  return(plot)
-}
-AddPointsToPlot = function(plot, ls){
-  list_names = names(ls)
-  data_table = data.frame(point.x = numeric(0),point.y = numeric(0), point.name = character(), stringsAsFactors = F)
-  for (i in 1:length(ls)){
-    data_table[i,1] = ls[[i]][1]
-    data_table[i,2] = ls[[i]][2]
-    data_table[i,3] = list_names[i]
-  }
-  plot = plot + 
-    geom_point(data = data_table, aes(point.x, point.y, color = point.name), size = 4)
   return(plot)
 }
